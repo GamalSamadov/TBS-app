9bdc991dcf3c3ebdf3343ec3a14552bf48c48b24
Gamal Samadov <gamalsamadov@gmail.com>
2023-03-29 17:07:01 +0300

    test 1

--- TBS_app/MudarrisViews.py
+++ TBS_app/MudarrisViews.py
@@ -3,7 +3,3 @@ from django.shortcuts import render, redirect
 
 def mudarris_asosiy(request):
     return render(request, 'mudarris_template/asosiy.html')
-
-
-def mudarris_kiritish(request):
-    return render(request, 'mudarris_template/asosiy.html')
